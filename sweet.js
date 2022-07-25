8107f66bbb27daf4f2868c3dd1e1744124243f10
Mauricio Perez <hudsonmaurio@gmail.com>
2022-07-24 22:50:14 -0300

    modlibr

--- sweet.js
+++ sweet.js
@@ -1,3 +1,9 @@
 function mostrarAlerta(){
-    alert('hola')
+    Swal.fire({
+        position: 'center',
+        icon: 'success',
+        title: 'Muchas gracias, te lo llevamos a la mesa!',
+        showConfirmButton: false,
+        timer: 1500
+      })
 }
