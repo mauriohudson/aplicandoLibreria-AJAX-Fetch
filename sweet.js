97766effabb6fb22dbbb09c3919f590462def7f4
Mauricio Perez <hudsonmaurio@gmail.com>
2022-07-25 12:28:09 -0300

    sweetal

--- sweet.js
+++ sweet.js
@@ -1,7 +1,10 @@
 document.getElementById('comprar-producto').addEventListener('click', function(){
-  Swal.fire(
-    'The Internet?',
-    'That thing is still around?',
-    'question'
-  )
+  Swal.fire({
+    title: 'Gracias',
+    text: 'Pronto le enviamos el cafe a su mesa',
+    imageUrl: 'https://unsplash.it/400/200',
+    imageWidth: 400,
+    imageHeight: 200,
+    imageAlt: 'Custom image',
+  })
 })
